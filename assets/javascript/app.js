2655f8369aed7cd9a2ccd5edeec9c3b2b6b3f652
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-08-28 16:29:58 -0500

    writing javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
     var topics = ["friends", "frasier", "the office", "mad men", "the sopranos"];
 
+    // function to display gifs to html
+    function displayGifs(){
+        var show = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=5Gi9X3cdkNgLAhoVIqyDKGgi6LMOx6Sc&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            var results = response.data;
+            for (var i = 0; i < results.length, i++){
+                var topicsDiv = $("<div>");
+                var rating = results[i].rating;
+                var p = $('<p>').text("Rating: " + rating);
+                var topicsImg = $('<img>');
+            }
+        })
+    }
+
 
     // create function to display tv show data in buttons
     function renderButtons(){
