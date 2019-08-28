982e681d22494fbfa643266426685ed03b61e072
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-08-28 15:34:24 -0500

    created buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
     var topics = ["friends", "frasier", "the office", "mad men", "the sopranos"];
+
+
+    // create function to display tv show data in buttons
+    function renderButtons(){
+        //no repeat buttons
+        $('#buttons-tv').empty();
+        // loop through array of shows
+        for (var i = 0; i < topics.length; i++){
+            var btn = $('<button>');                // generate button for each show in array
+            btn.addClass("tvShow");                 // add a class of tvShow to button
+            btn.attr("data-name", topics[i]);       // added a data attribute
+            btn.text(topics[i]);                    // initial button text
+            $('#buttons-tv').append(btn);           // append buttons to initial div
+        }
+    }
+
+    renderButtons();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
